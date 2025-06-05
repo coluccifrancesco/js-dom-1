0e57361278a62a5ccab8659fdcb205c9f9870c2e
coluccifrancesco <coluccif2002@gmail.com>
2025-06-05 17:13:38 +0200

    Refactor lamp toggle event handling to eliminate redundant variable declaration

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 const btn = document.getElementById('turnOn');
+let elemento = document.getElementById('lampImg');
 
 btn.addEventListener('click', 
     
-    function(){
-        let elemento = document.getElementById('lampImg');
+function(){
 
         if(btn.innerHTML === 'Accendi'){
-            btn.addEventListener('click', function(){
+            // btn.addEventListener('click', function(){
                 btn.innerHTML = 'Spegni'
                 elemento.src = '../img/yellow_lamp.png';
-                return
-            })
+                // return
+            // })
         }
 
-        if(btn.innerHTML === 'Spegni'){
-            btn.addEventListener('click', function(){
+        else {
+            // btn.addEventListener('click', function(){
                 elemento.src = '../img/white_lamp.png'
                 btn.innerHTML = 'Accendi'
-                return
-            })
+                // return
+            // })
         }
 
-        return
+        // return
     }
 )
 
