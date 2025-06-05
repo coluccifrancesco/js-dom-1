1a09882d56328e8c404cf66b4e5cfcdc7f44e443
coluccifrancesco <coluccif2002@gmail.com>
2025-06-05 16:40:37 +0200

    Refactor lamp toggle functionality to streamline button click event handling but has issue

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 const btn = document.getElementById('turnOn');
 
-btn.addEventListener('click', function(){
-    let elemento = document.getElementById('lampImg');
-    elemento.src = '../img/yellow_lamp.png';
+btn.addEventListener('click', 
     
-    return
-})
+    function(){
+        let elemento = document.getElementById('lampImg');
 
-// BONUS
-// Facciamo accendi e spegni:
-// Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
-// Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
-// E così via...
-// Per fare questo bonus potremmo aver bisogno di del metodo string.includes() 
+        if(btn.innerHTML === 'Accendi'){
+            btn.addEventListener('click', function(){
+                btn.innerHTML = 'Spegni'
+                elemento.src = '../img/yellow_lamp.png';
+                return
+            })
+        }
 
+        if(btn.innerHTML === 'Spegni'){
+            btn.addEventListener('click', function(){
+                elemento.src = '../img/white_lamp.png'
+                btn.innerHTML = 'Accendi'
+                return
+            })
+        }
+
+        return
+    }
+)
 
 //  http://127.0.0.1:3000/index.html
